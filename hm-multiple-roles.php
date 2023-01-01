5e7d0097c61ae058209c32af2756ed425f2014e4
Md. Hossni Mubarak <mhmrajib@gmail.com>
2023-01-02 03:58:43 +0600

    freemius sdk updated

--- hm-multiple-roles.php
+++ hm-multiple-roles.php
@@ -6,8 +6,13 @@
  * Version:		1.7
  * Author:		HM Plugin
  * Author URI:	https://hmplugin.com/
- * License:		GPL-2.0+
- * License URI:	http://www.gnu.org/licenses/gpl-2.0.txt
+ * Requires at least:   5.2
+ * Requires PHP:        7.2
+ * Tested up to:        6.1.1
+ * Text Domain:         hm-cool-author-box-widget
+ * Domain Path:         /languages/
+ * License:             GPLv2 or later
+ * License URI:         http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
